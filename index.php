46e00c4fdf7041f6cbbde49f39bfc43974f5790a
RandyTAR <br.randytar@gmail.com>
2024-08-06 10:40:27 -0700

    task one of my 30days challange

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            background-color: aqua;
+            font-size: 200px;
+            color: blanchedalmond;
+            text-align: center;
+            width: 100%;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+    </style>
+</head>
+<body>
+    <?php
+    echo "Hello World!";
+    ?>
+</body>
+</html>
