7cb4cc9a1f0a0730f5c8af623cff482c0b1ef2da
CEO of STAR Tech <154841250+Randy-TAR@users.noreply.github.com>
2024-08-14 13:51:26 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        body{
-            background-color: aqua;
-            font-size: 200px;
-            color: blanchedalmond;
-            text-align: center;
-            width: 100%;
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-        }
-    </style>
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php
-    echo "Hello World!";
-    ?>
+    <h1>
+    This is a guessing Game
+    </h1>
+    <a href="guessgame.php">Start Game</a>
 </body>
 </html>
+<?php
+if(isset($_GET['msg'])){
+    $message = $_GET['msg'];
+    echo''.$message.'';
+    urldecode($_GET['msg']);
+}
+session_destroy();
+?>
